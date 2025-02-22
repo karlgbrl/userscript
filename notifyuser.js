45fa186ba4d51b0098302f56f84cf6a4d77deffa
karlgbrl <155634584+karlgbrl@users.noreply.github.com>
2025-02-22 18:21:13 +0800

    Update notifyuser.js

--- notifyuser.js
+++ notifyuser.js
@@ -36,6 +36,8 @@ const myCSS = `
         font-family: 'Roboto', sans-serif;
         display: flex;
         align-items: center;
+        white-space: nowrap; /* Prevent text from wrapping to the next line */
+        overflow: hidden;    /* Hide any text that overflows the container */
         gap: 10px;
     }
 
@@ -43,6 +45,9 @@ const myCSS = `
         margin-bottom: 5px;
         font-size: 0.9em;
         font-family: 'Roboto', sans-serif;
+        white-space: nowrap; /* Prevent text from wrapping to the next line */
+        overflow: hidden;    /* Hide any text that overflows the container */
+        text-overflow: ellipsis; /* Display "..." for overflowed text */
     }
 
     .customcodenotify-buttons {
